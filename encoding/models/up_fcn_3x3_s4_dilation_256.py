9b15e07ae467cfe7b4eecff72ed730cee1df6dc5
yougoforward <908865817@qq.com>
2020-06-27 06:09:41 +0800

    localup2

--- encoding/models/up_fcn_3x3_s4_dilation_256.py
+++ encoding/models/up_fcn_3x3_s4_dilation_256.py
@@ -40,7 +40,7 @@ class up_fcn_3x3_s4_dilation_256(BaseNet):
 class up_fcn_3x3_s4_dilation_256Head(nn.Module):
     def __init__(self, in_channels, out_channels, norm_layer, up_kwargs):
         super(up_fcn_3x3_s4_dilation_256Head, self).__init__()
-        inter_channels = in_channels // 8
+        inter_channels = in_channels // 4
         self.conv5 = nn.Sequential(nn.Conv2d(in_channels, inter_channels, 3, padding=1, bias=False),
                                    norm_layer(inter_channels),
                                    nn.ReLU(),
@@ -77,7 +77,44 @@ class localUp(nn.Module):
                                    nn.ReLU()) 
         self._up_kwargs = up_kwargs
 
+class localUp2(nn.Module):
+    def __init__(self, in_channels1, in_channels2, norm_layer, up_kwargs):
+        super(localUp2, self).__init__()
+        self.key_dim = in_channels1//8
+        # self.refine = nn.Sequential(nn.Conv2d(256, 64, 3, padding=2, dilation=2, bias=False),
+        #                            norm_layer(64),
+        #                            nn.ReLU(),
+        #                            nn.Conv2d(64, 64, 3, padding=2, dilation=2, bias=False),
+        #                            norm_layer(64),
+        #                            nn.ReLU())
+        self.refine = nn.Sequential(nn.Conv2d(in_channels1, self.key_dim, 3, padding=2, dilation=2, bias=False),
+                                   norm_layer(in_channels1//8),
+                                   nn.ReLU(),
+                                   nn.Conv2d(in_channels1//8, self.key_dim, 3, padding=2, dilation=2, bias=False),
+                                   norm_layer(in_channels1//8),
+                                   )
+        self.refine2 = nn.Sequential(nn.Conv2d(in_channels2, self.key_dim, 1, padding=0, dilation=1, bias=False),
+                                   norm_layer(in_channels1//8),
+                                   ) 
+        self._up_kwargs = up_kwargs
 
+
+
+    def forward(self, c1,c2,out):
+        n,c,h,w =c1.size()
+        c1 = self.refine(c1) # n, 64, h, w
+        c2 = interpolate(c2, (h,w), **self._up_kwargs)
+        c2 = self.refine2(c2)
+
+        unfold_up_c2 = unfold(c2, 3, 2, 2, 1).view(n, -1, 3*3, h*w)
+        # torch.nn.functional.unfold(input, kernel_size, dilation=1, padding=0, stride=1)
+        energy = torch.matmul(c1.view(n, -1, 1, h*w).permute(0,3,2,1), unfold_up_c2.permute(0,3,1,2)) #n,h*w,1,3x3
+        att = torch.softmax(energy, dim=-1)
+        out = interpolate(out, (h,w), **self._up_kwargs)
+        unfold_out = unfold(out, 3, 2, 2, 1).view(n, -1, 3*3, h*w)
+        out = torch.matmul(att, unfold_out.permute(0,3,2,1)).permute(0,3,2,1).view(n,-1,h,w)
+
+        return out
 
     def forward(self, c1,c2,out):
         n,c,h,w =c1.size()
