4d8ac7f4ec3aa43eee40bc73b65f6c113ec7b050
yougoforward <908865817@qq.com>
2020-06-29 11:02:54 +0800

    s4

--- encoding/models/up_fcn_3x3_s4.py
+++ encoding/models/up_fcn_3x3_s4.py
@@ -62,22 +62,17 @@ class up_fcn_3x3_s4Head(nn.Module):
 class localUp(nn.Module):
     def __init__(self, in_channels1, in_channels2, norm_layer, up_kwargs):
         super(localUp, self).__init__()
-        self.key_dim = in_channels1//8
+        self.key_dim = in_channels1//4
         # self.refine = nn.Sequential(nn.Conv2d(256, 64, 3, padding=2, dilation=2, bias=False),
         #                            norm_layer(64),
         #                            nn.ReLU(),
         #                            nn.Conv2d(64, 64, 3, padding=2, dilation=2, bias=False),
         #                            norm_layer(64),
         #                            nn.ReLU())
-        self.refine = nn.Sequential(nn.Conv2d(in_channels1, self.key_dim, 3, padding=1, dilation=1, bias=False),
-                                   norm_layer(in_channels1//8),
-                                   nn.ReLU(),
-                                   nn.Conv2d(in_channels1//8, self.key_dim, 3, padding=1, dilation=1, bias=False),
-                                   norm_layer(in_channels1//8),
-                                   nn.ReLU())
+        self.refine = nn.Sequential(nn.Conv2d(in_channels1, self.key_dim, 1, padding=0, dilation=1, bias=False),
+                                   norm_layer(self.key_dim))
         self.refine2 = nn.Sequential(nn.Conv2d(in_channels2, self.key_dim, 1, padding=0, dilation=1, bias=False),
-                                   norm_layer(in_channels1//8),
-                                   nn.ReLU()) 
+                                   norm_layer(self.key_dim)) 
         self._up_kwargs = up_kwargs
 
 
