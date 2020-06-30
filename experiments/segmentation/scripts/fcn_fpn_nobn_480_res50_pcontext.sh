5667313c6cafaab114a0c913a473965975d7302f
yougoforward <908865817@qq.com>
2020-07-01 07:34:54 +0800

    s4

--- experiments/segmentation/scripts/fcn_fpn_nobn_480_res50_pcontext.sh
+++ experiments/segmentation/scripts/fcn_fpn_nobn_480_res50_pcontext.sh
@@ -6,10 +6,10 @@ python -m experiments.segmentation.train --dataset pcontext \
 
 #test [single-scale]
 python -m experiments.segmentation.test_whole --dataset pcontext \
-    --model fcn_fpn_nobn --aux --base-size 520 --crop-size 520 \
+    --model fcn_fpn_nobn --aux --base-size 520 --crop-size 480 \
     --backbone resnet50 --resume experiments/segmentation/runs/pcontext/fcn_fpn_nobn/fcn_fpn_nobn_480_res50_pcontext/model_best.pth.tar --split val --mode testval
 
 #test [multi-scale]
 python -m experiments.segmentation.test_whole --dataset pcontext \
-    --model fcn_fpn_nobn --aux --base-size 520 --crop-size 520 \
+    --model fcn_fpn_nobn --aux --base-size 520 --crop-size 480 \
     --backbone resnet50 --resume experiments/segmentation/runs/pcontext/fcn_fpn_nobn/fcn_fpn_nobn_480_res50_pcontext/model_best.pth.tar --split val --mode testval --ms
