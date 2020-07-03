50a1f90a4bb8d10b2cf961cb04ee6aa6acdacdaa
yougoforward <908865817@qq.com>
2020-07-03 22:16:23 +0800

    first commit

--- encoding/models/up_psp.py
+++ encoding/models/up_psp.py
@@ -12,7 +12,7 @@ from torch.nn.functional import interpolate, unfold
 
 from .base import BaseNet
 from .fcn import FCNHead
-from ..nn import PyramidPooling
+# from ..nn import PyramidPooling
 
 class up_psp(BaseNet):
     def __init__(self, nclass, backbone, aux=True, se_loss=False, norm_layer=nn.BatchNorm2d, **kwargs):
@@ -26,7 +26,7 @@ class up_psp(BaseNet):
         c1, c2, c3, c4 = self.base_forward(x)
 
         outputs = []
-        x = self.head(c1,c2,c4)
+        x = self.head(c1,c2,c3,c4)
         x = interpolate(x, (h,w), **self._up_kwargs)
         outputs.append(x)
         if self.aux:
@@ -40,39 +40,100 @@ class up_pspHead(nn.Module):
     def __init__(self, in_channels, out_channels, norm_layer, up_kwargs):
         super(up_pspHead, self).__init__()
         inter_channels = in_channels // 4
-        self.conv5 = nn.Sequential(PyramidPooling(in_channels, norm_layer, up_kwargs),
-                                   nn.Conv2d(in_channels * 2, inter_channels, 3, padding=1, bias=False),
+
+        self.psp = nn.Sequential(PyramidPooling(inter_channels, inter_channels, norm_layer, up_kwargs),
+                                   nn.Conv2d(inter_channels * 5, inter_channels, 3, padding=1, bias=False),
                                    norm_layer(inter_channels),
                                    nn.ReLU(True))
         self.conv6 = nn.Sequential(nn.Dropout2d(0.1, False),
                                    nn.Conv2d(inter_channels, out_channels, 1))
                                    
-        self.refine = nn.Sequential(nn.Conv2d(256, 64, 3, padding=2, dilation=2, bias=False),
-                                   norm_layer(64),
+        self._up_kwargs = up_kwargs
+        self.conv5 = nn.Sequential(nn.Conv2d(in_channels, inter_channels, 3, padding=1, bias=False),
+                                   norm_layer(inter_channels),
                                    nn.ReLU(),
-                                   nn.Conv2d(64, 64, 3, padding=2, dilation=2, bias=False),
-                                   norm_layer(64),
-                                   nn.ReLU())
-        self.refine2 = nn.Sequential(nn.Conv2d(512, 64, 1, padding=0, dilation=1, bias=False),
-                                   norm_layer(64),
-                                   nn.ReLU()) 
+                                   )
+        self.localUp2=localUp(256, 512, norm_layer, up_kwargs)
+        self.localUp3=localUp(512, 1024, norm_layer, up_kwargs)
+        self.localUp4=localUp(1024, 2048, norm_layer, up_kwargs)
+    def forward(self, c1,c2,c3,c4):
+        out = self.conv5(c4)
+        out = self.localUp4(c3, c40, out)
+        out = self.localUp3(c2, c30, out)
+        # out = self.localUp2(c1, c20, out)
+
+        out=self.psp(out)
+        return self.conv6(out)
+
+class PyramidPooling(Module):
+    """
+    Reference:
+        Zhao, Hengshuang, et al. *"Pyramid scene parsing network."*
+    """
+    def __init__(self, in_channels, out_channels, norm_layer, up_kwargs):
+        super(PyramidPooling, self).__init__()
+        self.pool1 = AdaptiveAvgPool2d(1)
+        self.pool2 = AdaptiveAvgPool2d(2)
+        self.pool3 = AdaptiveAvgPool2d(3)
+        self.pool4 = AdaptiveAvgPool2d(6)
+
+        # out_channels = int(in_channels/4)
+        self.conv1 = Sequential(Conv2d(in_channels, out_channels, 1, bias=False),
+                                norm_layer(out_channels),
+                                ReLU(True))
+        self.conv2 = Sequential(Conv2d(in_channels, out_channels, 1, bias=False),
+                                norm_layer(out_channels),
+                                ReLU(True))
+        self.conv3 = Sequential(Conv2d(in_channels, out_channels, 1, bias=False),
+                                norm_layer(out_channels),
+                                ReLU(True))
+        self.conv4 = Sequential(Conv2d(in_channels, out_channels, 1, bias=False),
+                                norm_layer(out_channels),
+                                ReLU(True))
+        # bilinear upsample options
         self._up_kwargs = up_kwargs
 
-    def forward(self, c1,c2,x):
+    def forward(self, x):
+        _, _, h, w = x.size()
+        feat1 = F.interpolate(self.conv1(self.pool1(x)), (h, w), **self._up_kwargs)
+        feat2 = F.interpolate(self.conv2(self.pool2(x)), (h, w), **self._up_kwargs)
+        feat3 = F.interpolate(self.conv3(self.pool3(x)), (h, w), **self._up_kwargs)
+        feat4 = F.interpolate(self.conv4(self.pool4(x)), (h, w), **self._up_kwargs)
+        return torch.cat((x, feat1, feat2, feat3, feat4), 1)
+
+class localUp(nn.Module):
+    def __init__(self, in_channels1, in_channels2, norm_layer, up_kwargs):
+        super(localUp, self).__init__()
+        self.key_dim = in_channels1//8
+        # self.refine = nn.Sequential(nn.Conv2d(256, 64, 3, padding=2, dilation=2, bias=False),
+        #                            norm_layer(64),
+        #                            nn.ReLU(),
+        #                            nn.Conv2d(64, 64, 3, padding=2, dilation=2, bias=False),
+        #                            norm_layer(64),
+        #                            nn.ReLU())
+        self.refine = nn.Sequential(nn.Conv2d(in_channels1, self.key_dim, 1, padding=0, dilation=1, bias=False),
+                                   norm_layer(in_channels1//8))
+        self.refine2 = nn.Sequential(nn.Conv2d(in_channels2, self.key_dim, 1, padding=0, dilation=1, bias=False),
+                                   norm_layer(in_channels1//8)) 
+        self._up_kwargs = up_kwargs
+
+
+
+    def forward(self, c1,c2,out):
         n,c,h,w =c1.size()
         c1 = self.refine(c1) # n, 64, h, w
         c2 = interpolate(c2, (h,w), **self._up_kwargs)
         c2 = self.refine2(c2)
 
-        unfold_up_c2 = unfold(c2, 3, 2, 2, 1).view(n, 64, 3*3, h*w)
+        unfold_up_c2 = unfold(c2, 3, 2, 2, 1).view(n, -1, 3*3, h*w)
         # torch.nn.functional.unfold(input, kernel_size, dilation=1, padding=0, stride=1)
-        energy = torch.matmul(c1.view(n, 64, 1, h*w).permute(0,3,2,1), unfold_up_c2.permute(0,3,1,2)) #n,h*w,1,3x3
+        energy = torch.matmul(c1.view(n, -1, 1, h*w).permute(0,3,2,1), unfold_up_c2.permute(0,3,1,2)) #n,h*w,1,3x3
         att = torch.softmax(energy, dim=-1)
-        out =self.conv5(x)
-        out0 = interpolate(out, (h,w), **self._up_kwargs)
-        unfold_out = unfold(out0, 3, 2, 2, 1).view(n, 512, 3*3, h*w)
-        out = torch.matmul(att, unfold_out.permute(0,3,2,1)).permute(0,3,2,1).view(n,512,h,w)
-        return self.conv6(out)
+        out = interpolate(out, (h,w), **self._up_kwargs)
+        unfold_out = unfold(out, 3, 2, 2, 1).view(n, -1, 3*3, h*w)
+        out = torch.matmul(att, unfold_out.permute(0,3,2,1)).permute(0,3,2,1).view(n,-1,h,w)
+
+        return out
 
 def get_up_psp(dataset='pascal_voc', backbone='resnet50', pretrained=False,
             root='~/.encoding/models', **kwargs):
