3b0d81954b07020199651ffec613ef707e9b6d60
yougoforward <908865817@qq.com>
2020-07-04 08:59:23 +0800

    off

--- encoding/models/fpn_psp.py
+++ encoding/models/fpn_psp.py
@@ -67,40 +67,40 @@ class fpn_pspHead(nn.Module):
         out=self.psp(out)
         return self.conv6(out)
 
-class PyramidPooling(Module):
+class PyramidPooling(nn.Module):
     """
     Reference:
         Zhao, Hengshuang, et al. *"Pyramid scene parsing network."*
     """
     def __init__(self, in_channels, out_channels, norm_layer, up_kwargs):
         super(PyramidPooling, self).__init__()
-        self.pool1 = AdaptiveAvgPool2d(1)
-        self.pool2 = AdaptiveAvgPool2d(2)
-        self.pool3 = AdaptiveAvgPool2d(3)
-        self.pool4 = AdaptiveAvgPool2d(6)
+        self.pool1 = nn.AdaptiveAvgPool2d(1)
+        self.pool2 = nn.AdaptiveAvgPool2d(2)
+        self.pool3 = nn.AdaptiveAvgPool2d(3)
+        self.pool4 = nn.AdaptiveAvgPool2d(6)
 
         # out_channels = int(in_channels/4)
-        self.conv1 = Sequential(Conv2d(in_channels, out_channels, 1, bias=False),
+        self.conv1 = nn.Sequential(nn.Conv2d(in_channels, out_channels, 1, bias=False),
                                 norm_layer(out_channels),
-                                ReLU(True))
-        self.conv2 = Sequential(Conv2d(in_channels, out_channels, 1, bias=False),
+                                nn.ReLU(True))
+        self.conv2 = nn.Sequential(nn.Conv2d(in_channels, out_channels, 1, bias=False),
                                 norm_layer(out_channels),
-                                ReLU(True))
-        self.conv3 = Sequential(Conv2d(in_channels, out_channels, 1, bias=False),
+                                nn.ReLU(True))
+        self.conv3 = nn.Sequential(nn.Conv2d(in_channels, out_channels, 1, bias=False),
                                 norm_layer(out_channels),
-                                ReLU(True))
-        self.conv4 = Sequential(Conv2d(in_channels, out_channels, 1, bias=False),
+                                nn.ReLU(True))
+        self.conv4 = nn.Sequential(nn.Conv2d(in_channels, out_channels, 1, bias=False),
                                 norm_layer(out_channels),
-                                ReLU(True))
+                                nn.ReLU(True))
         # bilinear upsample options
         self._up_kwargs = up_kwargs
 
     def forward(self, x):
         _, _, h, w = x.size()
-        feat1 = F.interpolate(self.conv1(self.pool1(x)), (h, w), **self._up_kwargs)
-        feat2 = F.interpolate(self.conv2(self.pool2(x)), (h, w), **self._up_kwargs)
-        feat3 = F.interpolate(self.conv3(self.pool3(x)), (h, w), **self._up_kwargs)
-        feat4 = F.interpolate(self.conv4(self.pool4(x)), (h, w), **self._up_kwargs)
+        feat1 = interpolate(self.conv1(self.pool1(x)), (h, w), **self._up_kwargs)
+        feat2 = interpolate(self.conv2(self.pool2(x)), (h, w), **self._up_kwargs)
+        feat3 = interpolate(self.conv3(self.pool3(x)), (h, w), **self._up_kwargs)
+        feat4 = interpolate(self.conv4(self.pool4(x)), (h, w), **self._up_kwargs)
         return torch.cat((x, feat1, feat2, feat3, feat4), 1)
 
 class localUp(nn.Module):
