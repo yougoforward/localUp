7b0c40f515f091595695248ef15c37caa704f8ca
yougoforward <908865817@qq.com>
2020-07-19 19:59:42 +0800

    dfpn61

--- experiments/segmentation/scripts/blur_detect_res101_focus_shi.sh
+++ experiments/segmentation/scripts/blur_detect_res101_focus_shi.sh
@@ -1,20 +1,20 @@
 # !/usr/bin/env bash
 # train
 python -m experiments.segmentation.train --dataset focus_shi \
-    --model blur_detect --aux --base-size 320 --crop-size 320 \
+    --model blur_detect --aux --base-size 321 --crop-size 321 \
     --backbone resnet101 --checkname blur_detect_res101_focus_shi
 
 #test [single-scale]
 python -m experiments.segmentation.test_whole --dataset focus_shi \
-    --model blur_detect --aux --base-size 320 --crop-size 320 \
+    --model blur_detect --aux --base-size 321 --crop-size 321 \
     --backbone resnet101 --resume experiments/segmentation/runs/focus_shi/blur_detect/blur_detect_res101_focus_shi/model_best.pth.tar --split val --mode testval
 
 #test [multi-scale]
 python -m experiments.segmentation.test_whole --dataset focus_shi \
-    --model blur_detect --aux --base-size 320 --crop-size 320 \
+    --model blur_detect --aux --base-size 321 --crop-size 321 \
     --backbone resnet101 --resume experiments/segmentation/runs/focus_shi/blur_detect/blur_detect_res101_focus_shi/model_best.pth.tar --split val --mode testval --ms
 
 #test [multi-scale]
 python -m experiments.segmentation.test_whole --dataset focus_shi \
-    --model blur_detect --aux --base-size 320 --crop-size 320 \
+    --model blur_detect --aux --base-size 321 --crop-size 321 \
     --backbone resnet101 --resume experiments/segmentation/runs/focus_shi/blur_detect/blur_detect_res101_focus_shi/model_best.pth.tar --split val --mode test --ms --save-folder experiments/segmentation/results/blur_detect_res101
