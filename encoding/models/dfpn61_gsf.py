9595812b2f87f0a02e68aac3739257e13ed37325
yougoforward <908865817@qq.com>
2020-07-19 10:38:13 +0800

    off

--- encoding/models/dfpn61_gsf.py
+++ encoding/models/dfpn61_gsf.py
@@ -7,14 +7,14 @@ import torch.nn.functional as F
 from .fcn import FCNHead
 from .base import BaseNet
 
-__all__ = ['dfpn6_gsf', 'get_dfpn6_gsf']
+__all__ = ['dfpn61_gsf', 'get_dfpn61_gsf']
 
 
-class dfpn6_gsf(BaseNet):
+class dfpn61_gsf(BaseNet):
     def __init__(self, nclass, backbone, aux=True, se_loss=False, norm_layer=nn.BatchNorm2d, **kwargs):
-        super(dfpn6_gsf, self).__init__(nclass, backbone, aux, se_loss, norm_layer=norm_layer, **kwargs)
+        super(dfpn61_gsf, self).__init__(nclass, backbone, aux, se_loss, norm_layer=norm_layer, **kwargs)
 
-        self.head = dfpn6_gsfHead(2048, nclass, norm_layer, se_loss, jpu=kwargs['jpu'], up_kwargs=self._up_kwargs)
+        self.head = dfpn61_gsfHead(2048, nclass, norm_layer, se_loss, jpu=kwargs['jpu'], up_kwargs=self._up_kwargs)
         if aux:
             self.auxlayer = FCNHead(1024, nclass, norm_layer)
 
@@ -32,10 +32,10 @@ class dfpn6_gsf(BaseNet):
 
 
 
-class dfpn6_gsfHead(nn.Module):
+class dfpn61_gsfHead(nn.Module):
     def __init__(self, in_channels, out_channels, norm_layer, se_loss, jpu=False, up_kwargs=None,
                  atrous_rates=(12, 24, 36)):
-        super(dfpn6_gsfHead, self).__init__()
+        super(dfpn61_gsfHead, self).__init__()
         self.se_loss = se_loss
         self._up_kwargs = up_kwargs
 
@@ -152,11 +152,11 @@ class localUp(nn.Module):
         return out
 
 
-def get_dfpn6_gsf(dataset='pascal_voc', backbone='resnet50', pretrained=False,
+def get_dfpn61_gsf(dataset='pascal_voc', backbone='resnet50', pretrained=False,
                  root='~/.encoding/models', **kwargs):
     # infer number of classes
     from ..datasets import datasets
-    model = dfpn6_gsf(datasets[dataset.lower()].NUM_CLASS, backbone=backbone, root=root, **kwargs)
+    model = dfpn61_gsf(datasets[dataset.lower()].NUM_CLASS, backbone=backbone, root=root, **kwargs)
     if pretrained:
         raise NotImplementedError
 
