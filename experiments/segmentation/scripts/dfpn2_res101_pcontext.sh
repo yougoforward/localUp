0c596b2e168902912300435b32ee38526f4a1c89
yougoforward <908865817@qq.com>
2020-07-19 20:54:18 +0800

    dfpn61

--- experiments/segmentation/scripts/dfpn2_res101_pcontext.sh
+++ experiments/segmentation/scripts/dfpn2_res101_pcontext.sh
@@ -1,6 +1,6 @@
 # !/usr/bin/env bash
 # train
-python -m experiments.segmentation.train_apex --dataset pcontext \
+python -m experiments.segmentation.train --dataset pcontext \
     --model dfpn2 --aux --base-size 520 --crop-size 520 \
     --backbone resnet101 --checkname dfpn2_res101_pcontext
 
