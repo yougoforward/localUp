e71eb0c1806bea028f9133de60f92416595dec73
yougoforward <908865817@qq.com>
2020-07-20 11:29:29 +0800

    off

--- experiments/segmentation/scripts/blur_dfpn_gsf_res50_focus_shi.sh
+++ experiments/segmentation/scripts/blur_dfpn_gsf_res50_focus_shi.sh
@@ -1,20 +1,20 @@
 # !/usr/bin/env bash
 # train
 python -m experiments.segmentation.train --dataset focus_shi \
-    --model blur_dfpn_gsf --aux --base-size 320 --crop-size 320 \
+    --model blur_dfpn_gsf --aux --base-size 321 --crop-size 321 \
     --backbone resnet50 --checkname blur_dfpn_gsf_res50_focus_shi
 
 #test [single-scale]
 python -m experiments.segmentation.test_whole --dataset focus_shi \
-    --model blur_dfpn_gsf --aux --base-size 320 --crop-size 320 \
+    --model blur_dfpn_gsf --aux --base-size 321 --crop-size 321 \
     --backbone resnet50 --resume experiments/segmentation/runs/focus_shi/blur_dfpn_gsf/blur_dfpn_gsf_res50_focus_shi/model_best.pth.tar --split val --mode testval
 
 #test [multi-scale]
 python -m experiments.segmentation.test_whole --dataset focus_shi \
-    --model blur_dfpn_gsf --aux --base-size 320 --crop-size 320 \
+    --model blur_dfpn_gsf --aux --base-size 321 --crop-size 321 \
     --backbone resnet50 --resume experiments/segmentation/runs/focus_shi/blur_dfpn_gsf/blur_dfpn_gsf_res50_focus_shi/model_best.pth.tar --split val --mode testval --ms
 
 #test [multi-scale]
 python -m experiments.segmentation.test_whole --dataset focus_shi \
-    --model blur_dfpn_gsf --aux --base-size 320 --crop-size 320 \
+    --model blur_dfpn_gsf --aux --base-size 321 --crop-size 321 \
     --backbone resnet50 --resume experiments/segmentation/runs/focus_shi/blur_dfpn_gsf/blur_dfpn_gsf_res50_focus_shi/model_best.pth.tar --split val --mode test --ms --save-folder experiments/segmentation/results/blur_dfpn_gsf_res50
