73c08f3ecaa0ca9471b251009c1733fa6245cc55
yougoforward <908865817@qq.com>
2020-07-21 11:45:37 +0800

    off

--- encoding/models/dfpn3_gsf.py
+++ encoding/models/dfpn3_gsf.py
@@ -112,14 +112,15 @@ class dfpn3_gsfHead(nn.Module):
 class localUp(nn.Module):
     def __init__(self, in_channels, out_channels, norm_layer, up_kwargs):
         super(localUp, self).__init__()
+        inter_channels = 256
         self.connect = nn.Sequential(
-                                   nn.Conv2d(in_channels, out_channels, 1, padding=0, dilation=1, bias=False),
+                                   nn.Conv2d(in_channels, inter_channels, 1, padding=0, dilation=1, bias=False),
                                    norm_layer(out_channels),
                                    nn.ReLU())
 
         self._up_kwargs = up_kwargs
         self.refine = nn.Sequential(
-                                   nn.Conv2d(out_channels, out_channels, 3, padding=1, dilation=1, bias=False),
+                                   nn.Conv2d(out_channels+inter_channels, out_channels, 3, padding=1, dilation=1, bias=False),
                                    norm_layer(out_channels),
                                    nn.ReLU(),
                                    nn.Conv2d(out_channels, out_channels, 3, padding=1, dilation=1, bias=False),
@@ -130,7 +131,7 @@ class localUp(nn.Module):
         n,c,h,w =c1.size()
         c1 = self.connect(c1) # n, 64, h, w
         c2 = F.interpolate(c2, (h,w), **self._up_kwargs)
-        out = c1+c2
+        out = torch.cat([c1,c2], dim=1)
         out = self.refine(out)
         return out
 
