fd05db69d6f7603b42f501d31199e2f6d79851bf
yougoforward <908865817@qq.com>
2020-07-22 23:25:13 +0800

    off

--- encoding/models/dfpn3_gsf.py
+++ encoding/models/dfpn3_gsf.py
@@ -55,17 +55,17 @@ class dfpn3_gsfHead(nn.Module):
         self.localUp3=localUp(512, inter_channels, norm_layer, up_kwargs)
         self.localUp4=localUp(1024, inter_channels, norm_layer, up_kwargs)
 
-        self.dconv2_1 = nn.Sequential(nn.Conv2d(in_channels, inter_channels, 3, padding=1, dilation=1, bias=False),
+        self.dconv2_1 = nn.Sequential(nn.Conv2d(inter_channels, inter_channels, 3, padding=1, dilation=1, bias=False),
                                    norm_layer(inter_channels),
                                    nn.ReLU()
                                    )
         self.dconv2_8 = nn.Sequential(
-                                   nn.Conv2d(in_channels, inter_channels, 3, padding=8, dilation=8, bias=False),
+                                   nn.Conv2d(inter_channels, inter_channels, 3, padding=8, dilation=8, bias=False),
                                    norm_layer(inter_channels),
                                    nn.ReLU(),
                                    )
         self.dconv3_8 = nn.Sequential(
-                                   nn.Conv2d(in_channels, inter_channels, 3, padding=8, dilation=8, bias=False),
+                                   nn.Conv2d(inter_channels, inter_channels, 3, padding=8, dilation=8, bias=False),
                                    norm_layer(inter_channels),
                                    nn.ReLU(),
                                    )
