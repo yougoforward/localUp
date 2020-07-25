b5615dfb9a1ad6ac33d40a237aa2e349b095bc31
yougoforward <908865817@qq.com>
2020-07-25 10:06:19 +0800

    off

--- encoding/models/dfpn_gsf.py
+++ encoding/models/dfpn_gsf.py
@@ -109,26 +109,85 @@ class dfpn_gsfHead(nn.Module):
 
         return self.conv6(out)
 
+# class localUp(nn.Module):
+#     def __init__(self, in_channels, out_channels, norm_layer, up_kwargs):
+#         super(localUp, self).__init__()
+#         self.connect = nn.Sequential(
+#                                    nn.Conv2d(in_channels, out_channels, 1, padding=0, dilation=1, bias=False),
+#                                    norm_layer(out_channels),
+#                                    nn.ReLU())
+
+
+#         self._up_kwargs = up_kwargs
+
+
+
+#     def forward(self, c1,c2):
+#         n,c,h,w =c1.size()
+#         c1 = self.connect(c1) # n, 64, h, w
+#         c2 = F.interpolate(c2, (h,w), **self._up_kwargs)
+#         out = c1+c2
+#         return out
 class localUp(nn.Module):
     def __init__(self, in_channels, out_channels, norm_layer, up_kwargs):
         super(localUp, self).__init__()
-        self.connect = nn.Sequential(
-                                   nn.Conv2d(in_channels, out_channels, 1, padding=0, dilation=1, bias=False),
-                                   norm_layer(out_channels),
-                                   nn.ReLU())
+        # self.connect = nn.Sequential(nn.Conv2d(in_channels, in_channels, 3, padding=1, dilation=1, bias=False),
+        #                            norm_layer(in_channels),
+        #                            nn.ReLU(),
+        #                            nn.Conv2d(in_channels, out_channels, 1, padding=0, dilation=1, bias=False),
+        #                            norm_layer(out_channels),
+        #                            nn.ReLU())
 
-
+        self.connect = Bottleneck(inplanes = in_channels, planes=in_channels//4, outplanes=out_channels, stride=1, dilation=1, norm_layer=norm_layer)
         self._up_kwargs = up_kwargs
-
-
 
     def forward(self, c1,c2):
         n,c,h,w =c1.size()
         c1 = self.connect(c1) # n, 64, h, w
         c2 = F.interpolate(c2, (h,w), **self._up_kwargs)
         out = c1+c2
+    
         return out
 
+class Bottleneck(nn.Module):
+    """ResNet Bottleneck
+    """
+    def __init__(self, inplanes, planes, outplanes, stride=1, dilation=1, norm_layer=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1 = norm_layer(planes)
+        self.conv2 = nn.Conv2d(
+            planes, planes, kernel_size=3, stride=stride,
+            padding=dilation, dilation=dilation, bias=False)
+        self.bn2 = norm_layer(planes)
+        self.conv3 = nn.Conv2d(
+            planes, outplanes, kernel_size=1, bias=False)
+        self.bn3 = norm_layer(outplanes)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.skip = nn.Sequential(
+                nn.Conv2d(inplanes, outplanes,
+                          kernel_size=1, stride=stride, bias=False),
+                norm_layer(outplanes),
+            )
+    def forward(self, x):
+        residual = self.skip(x)
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
 
 def get_dfpn_gsf(dataset='pascal_voc', backbone='resnet50', pretrained=False,
                  root='~/.encoding/models', **kwargs):
