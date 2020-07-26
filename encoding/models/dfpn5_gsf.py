4a62e0af3b3ca9837ed753151fe43d972eece467
yougoforward <908865817@qq.com>
2020-07-26 13:20:53 +0800

    off

--- encoding/models/dfpn5_gsf.py
+++ encoding/models/dfpn5_gsf.py
@@ -66,22 +66,22 @@ class dfpn5_gsfHead(nn.Module):
                                    norm_layer(inter_channels),
                                    nn.ReLU()
                                    )
-        self.dconv4_1 = nn.Sequential(nn.Conv2d(in_channels, inter_channels, 1, padding=0, dilation=1, bias=False),
+        self.dconv4_1 = nn.Sequential(nn.Conv2d(inter_channels, inter_channels, 1, padding=0, dilation=1, bias=False),
                                    norm_layer(inter_channels),
                                    nn.ReLU()
                                    )
         self.dconv4_2 = nn.Sequential(
-                                   nn.Conv2d(in_channels, inter_channels, 3, padding=2, dilation=2, bias=False),
+                                   nn.Conv2d(inter_channels, inter_channels, 3, padding=2, dilation=2, bias=False),
                                    norm_layer(inter_channels),
                                    nn.ReLU(),
                                    )
         self.dconv4_4 = nn.Sequential(
-                                   nn.Conv2d(in_channels, inter_channels, 3, padding=4, dilation=4, bias=False),
+                                   nn.Conv2d(inter_channels, inter_channels, 3, padding=4, dilation=4, bias=False),
                                    norm_layer(inter_channels),
                                    nn.ReLU(),
                                    )
         self.dconv4_8 = nn.Sequential(
-                                   nn.Conv2d(in_channels, inter_channels, 3, padding=8, dilation=8, bias=False),
+                                   nn.Conv2d(inter_channels, inter_channels, 3, padding=8, dilation=8, bias=False),
                                    norm_layer(inter_channels),
                                    nn.ReLU(),
                                    )
