2da739d749d6ef2a6ed18b7ea5a4e2e727b8fa83
yougoforward <908865817@qq.com>
2020-08-03 15:16:50 +0800

    71

--- encoding/models/dfpn71_gsf.py
+++ encoding/models/dfpn71_gsf.py
@@ -8,7 +8,7 @@ from .fcn import FCNHead
 from .base import BaseNet
 
 __all__ = ['dfpn71_gsf', 'get_dfpn71_gsf']
-
+7
 
 class dfpn71_gsf(BaseNet):
     def __init__(self, nclass, backbone, aux=True, se_loss=False, norm_layer=nn.BatchNorm2d, **kwargs):
@@ -56,8 +56,8 @@ class dfpn71_gsfHead(nn.Module):
         self.conv6 = nn.Sequential(nn.Dropout2d(0.1), nn.Conv2d(2*inter_channels, out_channels, 1))
 
         # self.localUp2=localUp(256, in_channels, norm_layer, up_kwargs)
-        self.localUp3=localUp(512, 256, inter_channels, norm_layer, up_kwargs)
-        self.localUp4=localUp(1024, 512, inter_channels, norm_layer, up_kwargs)
+        self.localUp3=localUp(512, inter_channels, norm_layer, up_kwargs)
+        self.localUp4=localUp(1024, inter_channels, norm_layer, up_kwargs)
 
         self.dconv4_1 = nn.Sequential(nn.Conv2d(in_channels, inter_channels, 1, padding=0, dilation=1, bias=False),
                                    norm_layer(inter_channels),
@@ -132,10 +132,10 @@ class dfpn71_gsfHead(nn.Module):
         return self.conv6(out)
 
 class localUp(nn.Module):
-    def __init__(self, in_channels, inter_channels, out_channels, norm_layer, up_kwargs):
+    def __init__(self, in_channels, out_channels, norm_layer, up_kwargs):
         super(localUp, self).__init__()
-        self.connect = nn.Sequential(nn.Conv2d(in_channels, inter_channels, 1, padding=0, dilation=1, bias=False),
-                                   norm_layer(inter_channels),
+        self.connect = nn.Sequential(nn.Conv2d(in_channels, out_channels, 1, padding=0, dilation=1, bias=False),
+                                   norm_layer(out_channels),
                                    nn.ReLU())
 
         self._up_kwargs = up_kwargs
@@ -143,69 +143,39 @@ class localUp(nn.Module):
         #                            norm_layer(out_channels),
         #                            nn.ReLU(),
         #                             )
-        self.refine = nn.Sequential(nn.Conv2d(inter_channels+out_channels, out_channels, 3, padding=1, dilation=1, bias=False),
-                                   norm_layer(out_channels),
+        inter_channels = out_channels//2
+        self.refine = nn.Sequential(nn.Conv2d(2*out_channels, inter_channels, 1, padding=0, dilation=1, bias=False),
+                                   norm_layer(inter_channels),
                                    nn.ReLU(),
+                                   nn.Conv2d(inter_channels, inter_channels, 3, padding=1, dilation=1, bias=False),
+                                   norm_layer(inter_channels),
+                                   nn.ReLU(),
+                                   nn.Conv2d(inter_channels, out_channels, 1, padding=0, dilation=1, bias=False),
+                                   norm_layer(out_channels),
                                     )
+        self.refine2 = nn.Sequential(nn.Conv2d(2*out_channels, inter_channels, 1, padding=0, dilation=1, bias=False),
+                                   norm_layer(inter_channels),
+                                   nn.ReLU(),
+                                   nn.Conv2d(inter_channels, inter_channels, 3, padding=2, dilation=2, bias=False),
+                                   norm_layer(inter_channels),
+                                   nn.ReLU(),
+                                   nn.Conv2d(inter_channels, out_channels, 1, padding=0, dilation=1, bias=False),
+                                   norm_layer(out_channels),
+                                    )
+        self.relu = nn.ReLU()                           
         # self.refine = Bottleneck(inplanes = 2*out_channels, planes=2*out_channels//4, outplanes=out_channels, stride=1, dilation=1, norm_layer=norm_layer)
     def forward(self, c1,c2):
         n,c,h,w =c1.size()
         c1 = self.connect(c1) # n, 64, h, w
         c2 = F.interpolate(c2, (h,w), **self._up_kwargs)
         out = torch.cat([c1,c2], dim=1)
-        out = self.refine(out)
+        out1 = self.refine(out)
+        out2 = self.refine2(out)
+
+        out = self.relu(c2+out1+out2)
         return out
 
-class Bottleneck(nn.Module):
-    """ResNet Bottleneck
-    """
-    def __init__(self, inplanes, planes, outplanes, stride=1, dilation=1, norm_layer=None):
-        super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
-        self.bn1 = norm_layer(planes)
-        
-        self.conv3 = nn.Conv2d(
-            planes, outplanes, kernel_size=1, bias=False)
-        self.bn3 = norm_layer(outplanes)
-        self.relu = nn.ReLU(inplace=True)
 
-        self.skip = nn.Sequential(
-                nn.Conv2d(inplanes, outplanes,
-                          kernel_size=1, stride=stride, bias=False),
-                norm_layer(outplanes),
-            )
-        self.dconv1 = nn.Sequential(nn.Conv2d(planes, planes, 3, padding=1, dilation=1, bias=False),
-                                   norm_layer(planes),
-                                   nn.ReLU(),
-                                   )
-        self.dconv2 = nn.Sequential(nn.Conv2d(planes, planes, 3, padding=2, dilation=2, bias=False),
-                                   norm_layer(planes),
-                                   nn.ReLU(),
-                                   )
-        self.dconv3 = nn.Sequential(nn.Conv2d(planes, planes, 3, padding=3, dilation=3, bias=False),
-                                   norm_layer(planes),
-                                   nn.ReLU(),
-                                   )                                  
-    def forward(self, x):
-        residual = self.skip(x)
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.dconv1(out)
-        # out2 = self.dconv2(out)
-        # out3 = self.dconv3(out)
-        
-        # out = out1+out2+out3
-
-        out = self.conv3(out)
-        out = self.bn3(out)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
 
 def get_dfpn71_gsf(dataset='pascal_voc', backbone='resnet50', pretrained=False,
                  root='~/.encoding/models', **kwargs):
