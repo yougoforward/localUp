4cc8d5f5bc447863e1d8ed5928762ba5003f2b0f
yougoforward <908865817@qq.com>
2020-08-05 09:39:16 +0800

    71

--- encoding/models/dfpn73_gsf.py
+++ encoding/models/dfpn73_gsf.py
@@ -145,9 +145,8 @@ class localUp(nn.Module):
         #                             )
         self.refine = nn.Sequential(nn.Conv2d(2*out_channels, out_channels, 3, padding=1, dilation=1, bias=False),
                                    norm_layer(out_channels),
-                                   nn.ReLU()
+                                   nn.ReLU(),
                                     )
-        # self.relu = nn.ReLU()                           
         # self.refine = Bottleneck(inplanes = 2*out_channels, planes=2*out_channels//4, outplanes=out_channels, stride=1, dilation=1, norm_layer=norm_layer)
     def forward(self, c1,c2):
         n,c,h,w =c1.size()
@@ -155,60 +154,8 @@ class localUp(nn.Module):
         c2 = F.interpolate(c2, (h,w), **self._up_kwargs)
         out = torch.cat([c1,c2], dim=1)
         out = self.refine(out)
-        # out = self.relu(c2+out)
-        out = c2+out
         return out
 
-class Bottleneck(nn.Module):
-    """ResNet Bottleneck
-    """
-    def __init__(self, inplanes, planes, outplanes, stride=1, dilation=1, norm_layer=None):
-        super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
-        self.bn1 = norm_layer(planes)
-        
-        self.conv3 = nn.Conv2d(
-            planes, outplanes, kernel_size=1, bias=False)
-        self.bn3 = norm_layer(outplanes)
-        self.relu = nn.ReLU(inplace=True)
-
-        self.skip = nn.Sequential(
-                nn.Conv2d(inplanes, outplanes,
-                          kernel_size=1, stride=stride, bias=False),
-                norm_layer(outplanes),
-            )
-        self.dconv1 = nn.Sequential(nn.Conv2d(planes, planes, 3, padding=1, dilation=1, bias=False),
-                                   norm_layer(planes),
-                                   nn.ReLU(),
-                                   )
-        self.dconv2 = nn.Sequential(nn.Conv2d(planes, planes, 3, padding=2, dilation=2, bias=False),
-                                   norm_layer(planes),
-                                   nn.ReLU(),
-                                   )
-        self.dconv3 = nn.Sequential(nn.Conv2d(planes, planes, 3, padding=3, dilation=3, bias=False),
-                                   norm_layer(planes),
-                                   nn.ReLU(),
-                                   )                                  
-    def forward(self, x):
-        residual = self.skip(x)
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.dconv1(out)
-        # out2 = self.dconv2(out)
-        # out3 = self.dconv3(out)
-        
-        # out = out1+out2+out3
-
-        out = self.conv3(out)
-        out = self.bn3(out)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
 
 def get_dfpn73_gsf(dataset='pascal_voc', backbone='resnet50', pretrained=False,
                  root='~/.encoding/models', **kwargs):
@@ -220,26 +167,42 @@ def get_dfpn73_gsf(dataset='pascal_voc', backbone='resnet50', pretrained=False,
 
     return model
 
+class PSPModule(nn.Module):
+    # (1, 2, 3, 6)
+    def __init__(self, sizes=(1, 3, 6, 8), dimension=2):
+        super(PSPModule, self).__init__()
+        self.stages = nn.ModuleList([self._make_stage(size, dimension) for size in sizes])
+        self.pool = nn.MaxPool2d(kernel_size=2)
 
+    def _make_stage(self, size, dimension=2):
+        if dimension == 1:
+            prior = nn.AdaptiveAvgPool1d(output_size=size)
+        elif dimension == 2:
+            prior = nn.AdaptiveAvgPool2d(output_size=(size, size))
+        elif dimension == 3:
+            prior = nn.AdaptiveAvgPool3d(output_size=(size, size, size))
+        return prior
+
+    def forward(self, feats):
+        n, c, _, _ = feats.size()
+        priors = [stage(feats).view(n, c, -1) for stage in self.stages]
+        priors.append(self.pool(feats).view(n, c, -1))
+        center = torch.cat(priors, -1)
+        return center
 class PAM_Module(nn.Module):
     """ Position attention module"""
     #Ref from SAGAN
-    def __init__(self, in_dim, key_dim, value_dim, out_dim, norm_layer):
+    def __init__(self, in_dim, key_dim, value_dim, out_dim, norm_layer, psp_size=(1,3,6,8)):
         super(PAM_Module, self).__init__()
         self.chanel_in = in_dim
+        self.psp = PSPModule(psp_size)
         self.pool = nn.MaxPool2d(kernel_size=2)
-        # self.pool = nn.AvgPool2d(kernel_size=2)
 
         self.query_conv = nn.Conv2d(in_channels=in_dim, out_channels=key_dim, kernel_size=1)
-        self.key_conv = nn.Conv2d(in_channels=in_dim, out_channels=key_dim, kernel_size=1)
-        # self.value_conv = nn.Conv2d(in_channels=value_dim, out_channels=value_dim, kernel_size=1)
-        # self.gamma = nn.Parameter(torch.zeros(1))
+        self.key_conv = nn.Conv1d(in_channels=in_dim, out_channels=key_dim, kernel_size=1)
         self.gamma = nn.Sequential(nn.Conv2d(in_channels=in_dim, out_channels=1, kernel_size=1, bias=True), nn.Sigmoid())
 
         self.softmax = nn.Softmax(dim=-1)
-        # self.fuse_conv = nn.Sequential(nn.Conv2d(value_dim, out_dim, 1, bias=False),
-        #                                norm_layer(out_dim),
-        #                                nn.ReLU(True))
 
     def forward(self, x):
         """
@@ -249,22 +212,126 @@ class PAM_Module(nn.Module):
                 out : attention value + input feature
                 attention: B X (HxW) X (HxW)
         """
-        xp = self.pool(x)
+        xp = self.psp(x)
         m_batchsize, C, height, width = x.size()
-        m_batchsize, C, hp, wp = xp.size()
+        m_batchsize, C, hpwp = xp.size()
         proj_query = self.query_conv(x).view(m_batchsize, -1, width*height).permute(0, 2, 1)
-        proj_key = self.key_conv(xp).view(m_batchsize, -1, wp*hp)
+        proj_key = self.key_conv(xp)
         energy = torch.bmm(proj_query, proj_key)
         attention = self.softmax(energy)
-        # proj_value = self.value_conv(x).view(m_batchsize, -1, width*height)
-        proj_value = xp.view(m_batchsize, -1, wp*hp)
+        proj_value = xp
         
         out = torch.bmm(proj_value, attention.permute(0, 2, 1))
-        out = out.view(m_batchsize, C, height, width)
-        # out = F.interpolate(out, (height, width), mode="bilinear", align_corners=True)
+        out = out.view(m_batchsize, -1, height, width)
 
         gamma = self.gamma(x)
         out = (1-gamma)*out + gamma*x
-        # out = self.fuse_conv(out)
         return out
+# class PAM_Module(nn.Module):
+#     """ Position attention module"""
+#     #Ref from SAGAN
+#     def __init__(self, in_dim, key_dim, value_dim, out_dim, norm_layer):
+#         super(PAM_Module, self).__init__()
+#         self.chanel_in = in_dim
+#         self.pool = nn.MaxPool2d(kernel_size=2)
+
+#         self.query_conv = nn.Conv2d(in_channels=in_dim, out_channels=key_dim, kernel_size=1)
+#         self.key_conv = nn.Conv2d(in_channels=in_dim, out_channels=key_dim, kernel_size=1)
+#         self.gamma = nn.Sequential(nn.Conv2d(in_channels=in_dim, out_channels=1, kernel_size=1, bias=True), nn.Sigmoid())
+
+#         self.softmax = nn.Softmax(dim=-1)
+
+#     def forward(self, x):
+#         """
+#             inputs :
+#                 x : input feature maps( B X C X H X W)
+#             returns :
+#                 out : attention value + input feature
+#                 attention: B X (HxW) X (HxW)
+#         """
+#         xp = self.pool(x)
+#         m_batchsize, C, height, width = x.size()
+#         m_batchsize, C, hp, wp = xp.size()
+#         proj_query = self.query_conv(x).view(m_batchsize, -1, width*height).permute(0, 2, 1)
+#         proj_key = self.key_conv(xp).view(m_batchsize, -1, wp*hp)
+#         energy = torch.bmm(proj_query, proj_key)
+#         attention = self.softmax(energy)
+#         # proj_value = self.value_conv(x).view(m_batchsize, -1, width*height)
+#         proj_value = xp.view(m_batchsize, -1, wp*hp)
+        
+#         out = torch.bmm(proj_value, attention.permute(0, 2, 1))
+#         out = out.view(m_batchsize, C, height, width)
+#         # out = F.interpolate(out, (height, width), mode="bilinear", align_corners=True)
+
+#         gamma = self.gamma(x)
+#         out = (1-gamma)*out + gamma*x
+#         # out = self.fuse_conv(out)
+#         return out
+
+# class PSPModule(nn.Module):
+#     # (1, 2, 3, 6)
+#     def __init__(self, sizes=(1, 3, 6, 8), dimension=2):
+#         super(PSPModule, self).__init__()
+#         self.stages = nn.ModuleList([self._make_stage(size, dimension) for size in sizes])
+
+#     def _make_stage(self, size, dimension=2):
+#         if dimension == 1:
+#             prior = nn.AdaptiveAvgPool1d(output_size=size)
+#         elif dimension == 2:
+#             prior = nn.AdaptiveAvgPool2d(output_size=(size, size))
+#         elif dimension == 3:
+#             prior = nn.AdaptiveAvgPool3d(output_size=(size, size, size))
+#         return prior
+
+#     def forward(self, feats):
+#         n, c, _, _ = feats.size()
+#         priors = [stage(feats).view(n, c, -1) for stage in self.stages]
+#         center = torch.cat(priors, -1)
+#         return center
+
+# class APAM_Module(nn.Module):
+#     """ Position attention module"""
+#     #Ref from SAGAN
+#     def __init__(self, in_dim, key_dim, value_dim, out_dim, norm_layer, psp_size=(1,3,6,8)):
+#         super(APAM_Module, self).__init__()
+#         self.chanel_in = in_dim
+#         self.key_channels = key_dim
+#         self.psp = PSPModule(psp_size)
+#         self.query_conv = nn.Sequential(
+#             nn.Conv2d(in_channels=in_dim, out_channels=key_dim, kernel_size=1, bias=False),
+#             norm_layer(key_dim),
+#             nn.ReLU(True))
+#         self.key_conv = self.query_conv
+#         self.value_conv = nn.Conv2d(in_channels=in_dim, out_channels=value_dim, kernel_size=1)
+#         self.gamma = nn.Sequential(nn.Conv2d(in_channels=in_dim, out_channels=1, kernel_size=1, bias=True), nn.Sigmoid())
+
+#         self.softmax = nn.Softmax(dim=-1)
+#         self.fuse_conv = nn.Sequential(nn.Conv2d(value_dim, out_dim, 1, bias=False),
+#                                        norm_layer(out_dim),
+#                                        nn.ReLU(True))
+
+#     def forward(self, x):
+#         """
+#             inputs :
+#                 x : input feature maps( B X C X H X W)
+#             returns :
+#                 out : attention value + input feature
+#                 attention: B X (HxW) X (HxW)
+#         """
+#         m_batchsize, C, height, width = x.size()
+#         proj_query = self.query_conv(x).view(m_batchsize, -1, width*height).permute(0, 2, 1)
+#         proj_key = self.psp(self.key_conv(x))
+#         energy = torch.bmm(proj_query, proj_key)
+
+#         energy = (self.key_channels ** -.5) * energy
+#         attention = self.softmax(energy)
+#         proj_value = self.psp(self.value_conv(x))
+        
+#         out = torch.bmm(proj_value, attention.permute(0, 2, 1))
+#         out = out.view(m_batchsize, -1, height, width)
+        
+#         out =self.fuse_conv(out)
+#         gamma = self.gamma(x)
+#         out = (1-gamma)*out + gamma*x
+#         return out
 
