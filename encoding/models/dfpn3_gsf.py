a6a4ed0354d1bd8663c8e48562918cb96de36f00
yougoforward <908865817@qq.com>
2020-08-06 08:44:16 +0800

    off

--- encoding/models/dfpn3_gsf.py
+++ encoding/models/dfpn3_gsf.py
@@ -140,7 +140,7 @@ class localUp(nn.Module):
 
         self._up_kwargs = up_kwargs
         self.refine = nn.Sequential(nn.Conv2d(2*out_channels, out_channels//2, 3, padding=1, dilation=1, bias=False),
-                                   norm_layer(out_channels),
+                                   norm_layer(out_channels//2),
                                    nn.ReLU(),
                                    nn.Conv2d(out_channels//2, out_channels, 1, padding=0, dilation=1, bias=False),
                                    norm_layer(out_channels),
