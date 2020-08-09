490d25fc1da3f7a4e20a9d1d60040ce9a73fcd8e
yougoforward <908865817@qq.com>
2020-08-09 10:58:44 +0800

    off

--- encoding/models/dfpn74_gsf.py
+++ encoding/models/dfpn74_gsf.py
@@ -139,25 +139,27 @@ class localUp(nn.Module):
                                    nn.ReLU())
 
         self._up_kwargs = up_kwargs
-        self.refine = nn.Sequential(nn.Conv2d(2*out_channels, 256, 1, padding=0, dilation=1, bias=False),
-                                   norm_layer(256),
-                                   nn.ReLU(),
-                                   nn.Conv2d(256, 256, 3, padding=1, dilation=1, bias=False),
-                                   norm_layer(256),
-                                   nn.ReLU(),
-                                   nn.Conv2d(256, out_channels, 1, padding=0, dilation=1, bias=False),
+        # self.refine = nn.Sequential(nn.Conv2d(2*out_channels, out_channels, 3, padding=1, dilation=1, bias=False),
+        #                            norm_layer(out_channels),
+        #                            nn.ReLU(),
+        #                             )
+        self.refine = nn.Sequential(nn.Conv2d(2*out_channels, out_channels, 3, padding=1, dilation=1, bias=False),
                                    norm_layer(out_channels),
                                     )
         self.relu = nn.ReLU()
-        self.residual = nn.Sequential(nn.Conv2d(2*out_channels, out_channels, 1, padding=0, dilation=1, bias=False),
-                                   norm_layer(out_channels))
+        self.refine2 = nn.Sequential(nn.Conv2d(out_channels, out_channels, 3, padding=1, dilation=1, bias=False),
+                                   norm_layer(out_channels), nn.ReLU()
+                                    )                           
+        # self.refine = Bottleneck(inplanes = 2*out_channels, planes=2*out_channels//4, outplanes=out_channels, stride=1, dilation=1, norm_layer=norm_layer)
     def forward(self, c1,c2):
         n,c,h,w =c1.size()
         c1 = self.connect(c1) # n, 64, h, w
         c2 = F.interpolate(c2, (h,w), **self._up_kwargs)
-        cat = torch.cat([c1,c2], dim=1)
-        out = self.refine(cat)
-        out = self.relu(self.residual(cat)+out)
+        out = torch.cat([c1,c2], dim=1)
+        out = self.refine(out)
+        out = self.relu(c2+out)
+        out = self.refine2(out)
+
         return out
 
 class Bottleneck(nn.Module):
