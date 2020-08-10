c93648f604f7fbe5d0672d3d4571fc57259a035f
yougoforward <908865817@qq.com>
2020-08-10 20:03:46 +0800

    off

--- encoding/models/dfpn2_gsf.py
+++ encoding/models/dfpn2_gsf.py
@@ -164,7 +164,7 @@ class localUp(nn.Module):
         unfold_c15 = F.unfold(c1, kernel_size=5, padding=2)
         c1_w5 = torch.matmul(unfold_c15.view(n, self.out_chs, 5*5, h*w).permute(0,1,3,2), self.w5)
         # c1 = c1_w5.view(n,self.out_chs,h,w)
-        c1 = (c1_w3+c1_w5).view(n,c,h,w)
+        c1 = (c1_w3+c1_w5).view(n,self.out_chs,h,w)
         c1 = self.bn_relu(c1)
         
         out = torch.cat([c1,c2], dim=1)
