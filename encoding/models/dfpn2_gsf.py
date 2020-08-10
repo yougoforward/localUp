4f6ddd75eb100598a46180289a33bb4f00b0ee6e
yougoforward <908865817@qq.com>
2020-08-10 19:51:40 +0800

    unfold

--- encoding/models/dfpn2_gsf.py
+++ encoding/models/dfpn2_gsf.py
@@ -151,7 +151,8 @@ class localUp(nn.Module):
                                    nn.ReLU(),
                                     )
         self.out_chs = out_channels
-
+        self.bn_relu = nn.Sequential(norm_layer(out_channels),
+                                   nn.ReLU())
     def forward(self, c1,c2):
         n,c,h,w =c1.size()
         c1 = self.connect(c1) # n, 64, h, w
@@ -165,7 +166,7 @@ class localUp(nn.Module):
         c1_w5 = torch.matmul(unfold_c15.view(n, self.out_chs, 5*5, h*w).permute(0,1,3,2), self.w5)
         # c1 = c1_w5.view(n,self.out_chs,h,w)
         c1 = (c1_w3+c1_w5).view(n,c,h,w)
-
+        c1 = self.bn_relu(c1)
         out = torch.cat([c1,c2], dim=1)
         out = self.refine(out)
         return out
