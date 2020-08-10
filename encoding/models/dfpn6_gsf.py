1c88fc6f1cc531116c01c55b312bcda429247bd4
yougoforward <908865817@qq.com>
2020-08-10 19:33:29 +0800

    unfold

--- encoding/models/dfpn6_gsf.py
+++ encoding/models/dfpn6_gsf.py
@@ -140,12 +140,16 @@ class localUp(nn.Module):
 
         self._up_kwargs = up_kwargs
 
-        self.w3 = nn.Parameter(torch.empty(3*3))
+        self.w31 = nn.Parameter(torch.empty(3*3))
         nn.init.normal_(self.w3)
-
+        self.w32 = nn.Parameter(torch.empty(3*3))
+        nn.init.normal_(self.w3)
         self.w5 = nn.Parameter(torch.empty(5*5))
         nn.init.normal_(self.w5)
-
+        self.bn_relu1 = nn.Sequential(norm_layer(out_channels),
+                                   nn.ReLU())
+        self.bn_relu2 = nn.Sequential(norm_layer(out_channels),
+                                   nn.ReLU())
         self.refine = nn.Sequential(nn.Conv2d(2*out_channels, out_channels, 1, padding=0, dilation=1, bias=False),
                                    norm_layer(out_channels),
                                    nn.ReLU(),
@@ -158,11 +162,18 @@ class localUp(nn.Module):
         c2 = F.interpolate(c2, (h,w), **self._up_kwargs)
         #
         unfold_c13 = F.unfold(c1, kernel_size=3, padding=1)
-        c1_w3 = torch.matmul(unfold_c13.view(n, self.out_chs, 3*3, h*w).permute(0,1,3,2), self.w3)
+        c1_w3 = torch.matmul(unfold_c13.view(n, self.out_chs, 3*3, h*w).permute(0,1,3,2), self.w31)
         # unfold_c15 = F.unfold(c1, kernel_size=5, padding=2)
         # c1_w5 = torch.matmul(unfold_c15.view(n, c, 5*5, h*w).permute(0,1,3,2), self.w5)
         c1 = c1_w3.view(n,self.out_chs,h,w)
         # c1 = (c1_w3+c1_w5).view(n,c,h,w)
+        c1 = self.bn_relu1(c1)
+
+        unfold_c23 = F.unfold(c2, kernel_size=3, padding=1)
+        c2_w3 = torch.matmul(unfold_c23.view(n, self.out_chs, 3*3, h*w).permute(0,1,3,2), self.w32)
+        c2 = c2_w3.view(n,self.out_chs,h,w)
+        c2 = self.bn_relu2(c2)
+
 
         out = torch.cat([c1,c2], dim=1)
         out = self.refine(out)
