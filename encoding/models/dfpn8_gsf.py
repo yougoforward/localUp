f776b5948ab935f8ddf1e68ed004044af1201751
yougoforward <908865817@qq.com>
2020-08-17 21:51:39 +0800

    dfpn4

--- encoding/models/dfpn8_gsf.py
+++ encoding/models/dfpn8_gsf.py
@@ -56,9 +56,9 @@ class dfpn8_gsfHead(nn.Module):
         self.localUp3=localUp(512, inter_channels, norm_layer, up_kwargs)
         self.localUp4=localUp(1024, inter_channels, norm_layer, up_kwargs)
 
-        self.context4 = Context(in_channels, inter_channels, inter_channels, 2, norm_layer)
-        self.context3 = Context(inter_channels, inter_channels, inter_channels, 4, norm_layer)
-        self.context2 = Context(inter_channels, inter_channels, inter_channels, 8, norm_layer)
+        self.context4 = Context(in_channels, inter_channels//2, inter_channels, 2, norm_layer)
+        self.context3 = Context(inter_channels, inter_channels//2, inter_channels, 4, norm_layer)
+        self.context2 = Context(inter_channels, inter_channels//2, inter_channels, 8, norm_layer)
 
         self.project = nn.Sequential(nn.Conv2d(3*inter_channels, inter_channels, 1, padding=0, dilation=1, bias=False),
                                    norm_layer(inter_channels),
