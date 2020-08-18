41264587e406be522fce9e72ee0dabf008bf4528
yougoforward <908865817@qq.com>
2020-08-18 11:54:04 +0800

    nogit

--- encoding/models/dfpn8_gsf.py
+++ encoding/models/dfpn8_gsf.py
@@ -74,12 +74,12 @@ class dfpn8_gsfHead(nn.Module):
 
         out2 = self.localUp3(c2, p3)
 
-        p2=self.context2(out2)
+        out=self.context2(out2)
 
         # out = self.localUp2(c1, p2)
-        p4 = F.interpolate(p4, (h,w), **self._up_kwargs)
-        p3 = F.interpolate(p3, (h,w), **self._up_kwargs)
-        out = self.project(torch.cat([p2, p3, p4], dim=1))
+        # p4 = F.interpolate(p4, (h,w), **self._up_kwargs)
+        # p3 = F.interpolate(p3, (h,w), **self._up_kwargs)
+        # out = self.project(torch.cat([p2, p3, p4], dim=1))
 
         #gp
         gp = self.gap(c4)        
