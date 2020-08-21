91b43291253f45a4eff9a54184a9eb5426ed6b2f
yougoforward <908865817@qq.com>
2020-08-21 14:20:42 +0800

    nogit

--- encoding/models/dfpn8_gsf.py
+++ encoding/models/dfpn8_gsf.py
@@ -40,7 +40,10 @@ class dfpn8_gsfHead(nn.Module):
         self._up_kwargs = up_kwargs
 
         inter_channels = in_channels // 4
-
+        self.conv5 = nn.Sequential(nn.Conv2d(in_channels, inter_channels, 3, padding=1, bias=False),
+                                   norm_layer(inter_channels),
+                                   nn.ReLU(),
+                                   )
         self.gap = nn.Sequential(nn.AdaptiveAvgPool2d(1),
                             nn.Conv2d(in_channels, inter_channels, 1, bias=False),
                             norm_layer(inter_channels),
@@ -56,9 +59,9 @@ class dfpn8_gsfHead(nn.Module):
         self.localUp3=localUp(512, inter_channels, norm_layer, up_kwargs)
         self.localUp4=localUp(1024, inter_channels, norm_layer, up_kwargs)
 
-        self.context4 = Context(in_channels, inter_channels//2, inter_channels, 2, norm_layer)
-        self.context3 = Context(inter_channels, inter_channels//2, inter_channels, 4, norm_layer)
-        self.context2 = Context(inter_channels, inter_channels//2, inter_channels, 8, norm_layer)
+        self.context4 = Context(inter_channels, inter_channels//2, inter_channels, 3, norm_layer)
+        self.context3 = Context(inter_channels, inter_channels//2, inter_channels, 6, norm_layer)
+        self.context2 = Context(inter_channels, inter_channels//2, inter_channels, 9, norm_layer)
 
         self.project = nn.Sequential(nn.Conv2d(3*inter_channels, inter_channels, 1, padding=0, dilation=1, bias=False),
                                    norm_layer(inter_channels),
@@ -66,14 +69,13 @@ class dfpn8_gsfHead(nn.Module):
                                    )
     def forward(self, c1,c2,c3,c4,c20,c30,c40):
         _,_, h,w = c2.size()
+        out4 = self.conv5(c4)
         p4=self.context4(c4)
 
         out3 = self.localUp4(c3, p4)
-
         p3=self.context3(out3)
 
         out2 = self.localUp3(c2, p3)
-
         out=self.context2(out2)
 
         # out = self.localUp2(c1, p2)
@@ -84,8 +86,8 @@ class dfpn8_gsfHead(nn.Module):
         #gp
         gp = self.gap(c4)        
         # se
-        se = self.se(gp)
-        out = out + se*out
+        # se = self.se(gp)
+        # out = out + se*out
 
         #non-local
         # out = self.gff(out)
