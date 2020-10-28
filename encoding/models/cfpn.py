9d118aa47ea21ad14bee1ca450e5ab82ce09afea
yougoforward <908865817@qq.com>
2020-10-28 13:08:26 +0800

    lu

--- encoding/models/cfpn.py
+++ encoding/models/cfpn.py
@@ -174,7 +174,7 @@ class localUp2(nn.Module):
         unfold_out = torch.stack([o1,o2,o3,o4], 3).permute(0,2,1,3)
         # out = torch.matmul(unfold_out, att.unsqueeze(3)).squeeze(3).permute(0,2,1).view(n,-1,hd,wd)
         # out = torch.mean(unfold_out, dim=3, keepdim=False).permute(0,2,1).view(n,-1,hd,wd)
-        out = F.interpolate(out, (hd wd), mode='bilinear', align_corners=True)
+        out = F.interpolate(out, (hd, wd), mode='bilinear', align_corners=True)
         return out
     
 class localUp(nn.Module):
