4a19f2a85dcdfe7102d660c0b645b2dd1c899893
yougoforward <908865817@qq.comm>
2020-11-05 16:42:14 +0800

    cfpn

--- encoding/models/cfpn_gsf.py
+++ encoding/models/cfpn_gsf.py
@@ -67,8 +67,8 @@ class cfpn_gsfHead(nn.Module):
                                    norm_layer(inter_channels), nn.ReLU())
         self.context2 = Context(inter_channels, inter_channels, inter_channels, 8, norm_layer)
 
-        self.project = nn.Sequential(nn.Conv2d(6*inter_channels, inter_channels//2, 1, padding=0, dilation=1, bias=False),
-                                   norm_layer(inter_channels//2),
+        self.project = nn.Sequential(nn.Conv2d(6*inter_channels, inter_channels, 1, padding=0, dilation=1, bias=False),
+                                   norm_layer(inter_channels),
                                    nn.ReLU(),
                                    )
         
