3567ed1964d7fa25144d7be800204dfa928aa220
yougoforward <908865817@qq.comm>
2020-11-05 16:39:16 +0800

    cfpn

--- encoding/models/cfpn_gsf.py
+++ encoding/models/cfpn_gsf.py
@@ -45,15 +45,16 @@ class cfpn_gsfHead(nn.Module):
         #                            nn.ReLU(),
         #                            )
         self.gap = nn.Sequential(nn.AdaptiveAvgPool2d(1),
-                            nn.Conv2d(in_channels, inter_channels//2, 1, bias=False),
-                            norm_layer(inter_channels//2),
+                            nn.Conv2d(in_channels, inter_channels, 1, bias=False),
+                            norm_layer(inter_channels),
                             nn.ReLU(True))
         self.se = nn.Sequential(
-                            nn.Conv2d(inter_channels//2, inter_channels//2, 1, bias=True),
+                            nn.Conv2d(inter_channels, inter_channels, 1, bias=True),
                             nn.Sigmoid())
-        self.gff = PAM_Module(in_dim=inter_channels//2, key_dim=inter_channels//8,value_dim=inter_channels//2,out_dim=inter_channels//2,norm_layer=norm_layer)
+        self.gff = PAM_Module(in_dim=inter_channels, key_dim=inter_channels//8,value_dim=inter_channels,
+                              out_dim=inter_channels,norm_layer=norm_layer)
 
-        self.conv6 = nn.Sequential(nn.Dropout2d(0.1), nn.Conv2d(inter_channels, out_channels, 1))
+        self.conv6 = nn.Sequential(nn.Dropout2d(0.1), nn.Conv2d(2*inter_channels, out_channels, 1))
 
         self.localUp3=localUp(512, inter_channels, norm_layer, up_kwargs)
         self.localUp4=localUp(1024, inter_channels, norm_layer, up_kwargs)
